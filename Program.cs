92612fa92037841bce3fd65c0d1e61a848289a60
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-15 12:05:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Google.Cloud.Compute.V1;
+
+// Instantiate the MachineTypesClient and ImagesClient
+MachineTypesClient machineTypesClient = MachineTypesClient.Create();
+ImagesClient imagesClient = ImagesClient.Create();
+
+// List machine types
+var machineTypesList = machineTypesClient.List(new ListMachineTypesRequest
+{
+    Project = "focus-cache-387205",
+    Zone = "europe-southwest1-a"
+});
+
+Console.WriteLine("Machine Types:");
+foreach (MachineType machineType in machineTypesList)
+{
+    Console.WriteLine($"- {machineType.Name}");
+}
+
+Console.WriteLine();
